0dc1ec791102afc909366732952357626c045c22
Brandon <Brandon Sissenah>
2019-12-08 15:17:10 -0500

    added new images to homepage (20 png's)

--- js/main.js
+++ js/main.js
@@ -31,42 +31,102 @@
     size: `250ml`,
     price: 90
   }, {
-    img: `iimg/i6.png`,
+    img: `img/i6.png`,
     name: `Resolute Shower Oil`,
-    description: `tril`,
+    description: `Coffee`,
     size: `250ml`,
     price: 90
   }, {
     img: `img/i7.png`,
     name: `Resolute Shower Oil`,
-    description: `tril`,
+    description: `live`,
     size: `250ml`,
     price: 90
   }, {
     img: `img/i8.png`,
     name: `Resolute Shower Oil`,
-    description: `tril`,
+    description: `hike`,
     size: `250ml`,
     price: 90
   }, {
     img: `img/i10.png`,
     name: `Resolute Shower Oil`,
-    description: `tril`,
+    description: `salt`,
     size: `250ml`,
     price: 90
   }, {
     img: `img/i11.png`,
     name: `Resolute Shower Oil`,
+    description: `sandalwood`,
+    size: `250ml`,
+    price: 90
+  }, {
+    img: `img/i12.png`,
+    name: `Resolute Hand Creame`,
+    description: `Bergamont Rind`,
+    size: `250ml`,
+    price: 90
+  }, {
+    img: `img/i13.png`,
+    name: `Resolute Hand Soap`,
+    description: `Noir Rose`,
+    size: `250ml`,
+    price: 60
+  }, {
+    img: `img/i14.png`,
+    name: `Resolute Aftershave`,
+    description: `Seoul`,
+    size: `250ml`,
+    price: 70
+  }, {
+    img: `img/i15.png`,
+    name: `Resolute Body Wash`,
+    description: `Vert`,
+    size: `250ml`,
+    price: 90
+  }, {
+    img: `img/i16.png`,
+    name: `Resolute Shower Oil`,
     description: `tril`,
     size: `250ml`,
     price: 90
-  }
+  }, {
+    img: `img/i17.png`,
+    name: `Resolute Shower Oil`,
+    description: `Coffee`,
+    size: `250ml`,
+    price: 90
+  }, {
+    img: `img/i18.png`,
+    name: `Resolute Shower Oil`,
+    description: `live`,
+    size: `250ml`,
+    price: 90
+  }, {
+    img: `img/i19.png`,
+    name: `Resolute Shower Oil`,
+    description: `hike`,
+    size: `250ml`,
+    price: 90
+  }, {
+    img: `img/i20.png`,
+    name: `Resolute Shower Oil`,
+    description: `salt`,
+    size: `250ml`,
+    price: 90
+  }, {
+    img: `img/i1.png`,
+    name: `Resolute Shower Oil`,
+    description: `sandalwood`,
+    size: `250ml`,
+    price: 90
+  },
 ];
 
   function getItemAsHtmlString(item) {
   return `<div> 
               <img src=${item.img} width="230px" height="230px">
-              <a><strong>${item.name}</strong></a>
+              <h3><strong>${item.name}</strong></h3>
               <a><strong>${item.description}</strong></a>
               <a><strong>${item.size}</strong></a>
               <a>$<strong>${item.price}</strong></a>
@@ -85,7 +145,7 @@
       document.getElementById(`item`).innerHTML = strOfHtml;
     }
 
-    renderItem(item.slice(0, 5));
+    renderItem(item.slice(0, 20));
 
     document.getElementById('layout').addEventListener('click', event => changeGrids() )
 
