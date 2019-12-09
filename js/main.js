31ed40408a92ce7816425522e615b06093da69cc
Brandon <Brandon Sissenah>
2019-12-08 21:28:44 -0500

    fixed sort by category

--- js/main.js
+++ js/main.js
@@ -1,143 +1,173 @@
 const item = [
   {
     img: `img/i1.png`,
-    name: `Resolute Hand Creame`,
+    category: `skincare`,
+    name: `Resolute Hand Cream`,
     description: `Bergamont Rind`,
     size: `250ml`,
-    price: 90
+    cost: 90
   }, {
     img: `img/i2.png`,
-    name: `Resolute Hand Soap`,
+    category: `skincare`,
+    name: `Resolute Hand Cream`,
     description: `Noir Rose`,
     size: `250ml`,
-    price: 60
+    cost: 60
   }, {
     img: `img/i3.png`,
+    category: `skincare`,
     name: `Resolute Aftershave`,
     description: `Seoul`,
     size: `250ml`,
-    price: 70
+    cost: 70
   }, {
     img: `img/i4.png`,
-    name: `Resolute Body Wash`,
+    category: `skincare`,
+    name: `Resolute Body Cream`,
     description: `Vert`,
     size: `250ml`,
-    price: 90
+    cost: 95
   }, {
     img: `img/i11.png`,
-    name: `Resolute Shower Oil`,
-    description: `tril`,
+    category: `skincare`,
+    name: `Resolute Body Oil`,
+    description: `Sandalwood`,
     size: `250ml`,
-    price: 90
+    cost: 98
   }, {
-    img: `iimg/i6.png`,
     img: `img/i6.png`,
-    name: `Resolute Shower Oil`,
-    description: `tril`,
+    category: `skincare`,
+    name: `Resolute face cream`,
     description: `Coffee`,
     size: `250ml`,
-    price: 90
+    cost: 90
   }, {
     img: `img/i7.png`,
-    name: `Resolute Shower Oil`,
-    description: `tril`,
+    category: `skincare`,
+    name: `Resolute Lip Cream`,
     description: `live`,
     size: `250ml`,
-    price: 90
+    cost: 90
   }, {
     img: `img/i8.png`,
-    name: `Resolute Shower Oil`,
-    description: `tril`,
+    category: `skincare`,
+    name: `Resolute moisturizer`,
     description: `hike`,
     size: `250ml`,
-    price: 90
+    cost: 90
   }, {
     img: `img/i10.png`,
+    category: `shower`,
     name: `Resolute Shower Oil`,
     description: `tril`,
     description: `salt`,
     size: `250ml`,
-    price: 90
+    cost: 50
   }, {
     img: `img/i11.png`,
+    category: `shower`,
     name: `Resolute Shower Oil`,
     description: `sandalwood`,
     size: `250ml`,
-    price: 90
+    cost: 50
   }, {
     img: `img/i12.png`,
+    category: `skincare`,
     name: `Resolute Hand Creame`,
-    description: `Bergamont Rind`,
+    description: `Lemon Rind`,
     size: `250ml`,
-    price: 90
+    cost: 97
   }, {
     img: `img/i13.png`,
+    category: `shower`,
     name: `Resolute Hand Soap`,
     description: `Noir Rose`,
     size: `250ml`,
-    price: 60
+    cost: 35
   }, {
     img: `img/i14.png`,
+    category: `skincare`,
     name: `Resolute Aftershave`,
-    description: `Seoul`,
+    description: `Basil`,
     size: `250ml`,
-    price: 70
+    cost: 70
   }, {
     img: `img/i15.png`,
+    category: `shower`,
     name: `Resolute Body Wash`,
-    description: `Vert`,
+    description: `Day`,
     size: `250ml`,
-    price: 90
+    cost: 50
   }, {
     img: `img/i16.png`,
+    category: `shower`,
     name: `Resolute Shower Oil`,
-    description: `tril`,
+    description: `Sea Mist`,
     size: `250ml`,
-    price: 90
+    cost: 40
   }, {
     img: `img/i17.png`,
+    category: `skincare`,
     name: `Resolute Shower Oil`,
     description: `Coffee`,
     size: `250ml`,
     price: 90
   }, {
     img: `img/i18.png`,
+    category: `skincare`,
     name: `Resolute Shower Oil`,
     description: `live`,
     size: `250ml`,
-    price: 90
+    cost: 120
   }, {
     img: `img/i19.png`,
+    category: `skincare`,
     name: `Resolute Shower Oil`,
     description: `hike`,
     size: `250ml`,
-    price: 90
+    cost: 130
   }, {
     img: `img/i20.png`,
-    name: `Resolute Shower Oil`,
+    category: `skincare`,
+    name: `Resolute Soap Bar`,
     description: `salt`,
     size: `250ml`,
-    price: 90
+    cost: 90
   }, {
     img: `img/i1.png`,
+    category: `skincare`,
     name: `Resolute Shower Oil`,
     description: `sandalwood`,
     size: `250ml`,
-    price: 90
+    cost: 90
   },
 ];
 
-  function getItemAsHtmlString(item) {
-  return `<div> 
-              <img src=${item.img} width="230px" height="230px">
-              <h3><strong>${item.name}</strong></h3>
-              <a><strong>${item.description}</strong></a>
-              <a><strong>${item.size}</strong></a>
-              <a>$<strong>${item.price}</strong></a>
-              <a class="button" href="product.html">Add to bag</a>
-        </div>
-        <br>`
-  }
+
+function getItemAsHtmlString(item) {
+return `<article class="item ${item.category}">
+<div><h3>${item.name}</h3>
+<img src=${item.img} width="230px" height="230px">
+<a><strong>${item.description}</strong></a>
+<a><strong>${item.size}</strong></a>
+<a class="item-cost">$<strong>${item.cost}</strong></a>
+<a class="button" href="product.html">Add to bag</a></div>
+</article>`;
+}
+
+
+
+  // function getItemAsHtmlString(item) {
+  // return `<div> 
+  //             <img src=${item.img} width="230px" height="230px">
+  //             <h3><strong>${item.name}</strong></h3>
+  //             <a><strong>${item.description}</strong></a>
+  //             <a><strong>${item.size}</strong></a>
+  //             <a>$<strong>${item.cost}</strong></a>
+  //             <a class="button" href="product.html">Add to bag</a>
+  //       </div>
+  //       <br>`
+  // }
   
   function changeGrids() {
     document.getElementById(`item`).classList.toggle(`grid-toggle`);
@@ -150,7 +180,16 @@ const item = [
 
     renderItem(item.slice(0, 20));
 
-    document.getElementById('layout').addEventListener('click', event => changeGrids() )
+    console.table(item);
     
+    function checkCategory(event) {
+      const userSearch = event.target.value;
 
-    
+      const matchingCategories = item.filter(item => item.category == userSearch);
+      renderItem(matchingCategories)
+    }
+
+    document.getElementById('layout').addEventListener('click', event => changeGrids() )
+    document.getElementById('itemCategory').addEventListener('click',event => checkCategory(event))
+
+
